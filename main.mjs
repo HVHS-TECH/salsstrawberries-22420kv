3e4766f7308336453f7286de944dd5196c86fe14
22420kv <22420kv@hvhs.school.nz>
2025-05-19 00:27:14 +0000

    aaaa

--- main.mjs
+++ main.mjs
@@ -1,1 +1,10 @@
+import { fb_initialise, fb_authenticate, fb_write }
+    from './script.mjs';
+    window.fb_initialise = fb_initialise;
+    window.fb_authenticate  = fb_authenticate;
+    window.fb_write = fb_write;
+
+var name = document.getElementById("name").value;
+var favoriteFruit = document.getElementById("favoriteFruit").value;
+var fruitQuantity = document.getElementById("fruitQuantity").value;
 
--- script.mjs
+++ script.mjs
@@ -1,1 +1,109 @@
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log('%c fb_io.mjs',
+            'color: blue; background-color: white;');
 
+import { initializeApp }
+ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
+import { getAuth, GoogleAuthProvider, signInWithPopup }
+ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+import { ref, set }
+ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
+export { fb_initialise, fb_authenticate, fb_write };
+const FB_GAMECONFIG = {
+        apiKey: "AIzaSyCn36qBrPRutqLXCYIyzkyjMQRiYyhRC2Q",
+        authDomain: "comp-2025-kyla-van-weele.firebaseapp.com",
+        databaseURL: "https://comp-2025-kyla-van-weele-default-rtdb.firebaseio.com",
+        projectId: "comp-2025-kyla-van-weele",
+        storageBucket: "comp-2025-kyla-van-weele.firebasestorage.app",
+        messagingSenderId: "726085363137",
+        appId: "1:726085363137:web:32da18f88b84bf19fffb40",
+        measurementId: "G-RXDD9GFN2H"
+      };
+
+starting();
+
+function starting() {
+    starting = alert("Starting... \nClick OK");
+    if(starting == null) {
+        fb_initialise();
+        return(starting);
+    }
+}
+
+/***********************************/
+// fb_initialise()
+// Called by initialise firebase Button
+// Firebase - using CDN
+// Input: n/a
+// Return: n/a
+/***********************************/
+var FB_GAMEDB;
+function fb_initialise() {
+    console.log('%c fb_initialise(): ', 
+        'color: ' + COL_C + '; background-color: red'
+    );
+    const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
+    FB_GAMEDB  = getDatabase(FB_GAMEAPP);
+    console.info(FB_GAMEDB);  //DIAG
+   
+}
+
+
+/***********************************/
+// fb_authenticate()
+// Called by authenticate Button
+// To firebase - cia signInwIthPopup
+// Input: n/a
+// Return: n/a
+/***********************************/
+function fb_authenticate() {
+    console.log('%c fb_authenticate(): ', 
+        'color: ' + COL_C + '; background-color: blue'
+    );
+
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+    // The following makes Google ask the user to select the account
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        console.log('successful login');
+        //✅ Code for a successful authentication goes here
+    })
+
+    .catch((error) => {
+        console.log('failed login');
+        //❌ Code for an authentication error goes here
+    });
+}
+
+
+/***********************************/
+// fb_writeRecord()
+// Called by write record Button
+// A record to firebase - via set
+// Input: n/a
+// Return: n/a
+/***********************************/
+function fb_write() {
+    console.log('%c fb_write(): ',
+        'color: ' + COL_C + '; background-color: orange'
+    );
+var data_to_write = {Pets: 1, Plants: 5}
+    const writeRecord = ref(FB_GAMEDB, 'House/People');
+    set(writeRecord, data_to_write).then(() => {  
+        console.log('successfull write');
+        //✅ Code for a successful write goes here
+    }).catch((error) => {
+        console.log(error);
+        //❌ Code for a write error goes here
+    });
+}
